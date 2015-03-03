a81175f679eb79c353fe173b15477cecb91f8e1e
dhirajranjit <ranjitd11@mycu.concord.edu>
2015-03-03 09:55:23 -0500

    gitPractice.c

---
+++ gitPractice.c
@@ -0,0 +1,40 @@
+/*
+CS 240 Git Practice File
+Add a statement that prints your name in main
+*/
+
+void Preston(int);
+void Bruno();
+void Dhiraj();
+
+#include <stdio.h>
+
+int main()
+{
+   puts("Lonnie Bowe");
+   Preston(0);
+   Bruno();
+   return 0;
+}
+
+void Preston(int index)
+{
+   char name[] = "Preston Bennett\n";
+
+   if (name[index] == '\0')
+   { return; }
+
+   else
+   {
+      printf("%c", name[index]);
+      Preston(index + 1);
+   }
+}
+
+void Bruno(){
+   printf("Bruno Jahel\n");
+}
+
+void Dhiraj(){
+   printf("Dhiraj Ranjit\n");
+}
